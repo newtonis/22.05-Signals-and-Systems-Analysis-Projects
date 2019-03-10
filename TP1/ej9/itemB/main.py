17e7089236aa210c66520cbd8374a39621533e96
Ariel Nowik <newtonis.penguin@gmail.com>
2019-03-10 06:34:00 -0700

    add

--- TP1/ej9/itemB/main.py
+++ TP1/ej9/itemB/main.py
@@ -2,36 +2,52 @@ from math import pi, sin
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.fft import fft
+import matplotlib.patches as mpatches
 import symbol as sp
-
-x = []
-y = []
-
-M = 4
-
-x_range = range(-50, 50+1)
-y_range = range(-int(50/4), int(50/4+1))
-
-Nx = len(x_range)
-Ny = len(y_range)
-
-x = [sin(2 * pi * 0.125 / 2.0 * n) for n in x_range]
-
-y = [x[n*4] for n in y_range]
+from util_python import datacursor_easy
 
 
-tx = abs(fft(x)/Nx) # rafa
-ty = abs(fft(y)/Ny) # rafa
+def plot_data():
+    fig, ax1 = plt.subplots()
+    patches = []
 
-print(tx)
+    x = []
+    y = []
 
-int_tx = [int(i) for i in ty]
+    M = 4
+
+    x_range = range(-50, 50+1)
+    y_range = range(-int(50/4), int(50/4)+1)
+
+    rx = range(101)
+    ry = range(0, 100, 4)
+
+    Nx = len(x_range)
+    Ny = len(y_range)
+
+    x = [sin(2*pi * 0.25 * n) for n in x_range]
+
+    y = [x[4*n] for n in y_range]
+
+    tx = abs(fft(x)/Nx) # rafa
+    ty = abs(fft(y)/Ny) # rafa
+
+    ax1.plot(rx, tx, color='blue')
+    ax1.plot(ry, ty, color='red')
+
+    patches.append(mpatches.Patch(color="blue", label="original"))
+    patches.append(mpatches.Patch(color="red", label="downsampled"))
+
+    plt.legend(handles=patches)
+    plt.xlabel("Armpónico (n)")
+    plt.ylabel("Valor")
+
+    plt.minorticks_on()
+    plt.grid(which='major', linestyle='-', linewidth=0.3, color='black')
+    plt.grid(which='minor', linestyle=':', linewidth=0.1, color='black')
+
+    plt.show()
 
 
 
-plt.plot(x_range, tx, color='blue')
-plt.plot(y_range, ty, color='red')
-
-
-
-plt.show()
+plot_data()
