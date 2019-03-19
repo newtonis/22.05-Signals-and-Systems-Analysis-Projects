93404659401d41acd92f7876492c217819b8e6d0
newtonis penguin <newtonis.penguin@gmail.com>
2019-03-19 16:08:54 -0300

    add

--- TP1/simulacion_ej5/XmlUnitTest.py
+++ TP1/simulacion_ej5/XmlUnitTest.py
@@ -1,9 +1,9 @@
 from Etapas.SignalsReadWrite import readSignal
 from Signals import SignalGenerator
 
-signal = readSignal("Signals/sine.xml")
+#signal = readSignal("Signals/sine.xml")
 
-print(signal.xvar, signal.values)
+#print(signal.xvar, signal.values)
 
 #SignalGenerator.GenerateSine(20000/12.0, 0, 0.005)
 #SignalGenerator.GenerateCos(20000*12.0, 0, 0.005)
