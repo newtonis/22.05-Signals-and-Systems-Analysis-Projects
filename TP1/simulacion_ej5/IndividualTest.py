d4fe767459e8ed7857d447fcca047c0d6611dbd6
rocioparra <rparra@itba.edu.ar>
2019-04-03 17:45:23 -0300

    matenme

--- TP1/simulacion_ej5/IndividualTest.py
+++ TP1/simulacion_ej5/IndividualTest.py
@@ -10,6 +10,6 @@ from IndividualTests.espectros import spectra_plot
 
 
 # joaco_spectrum()
-spectra_plot("ExpressInput/Mediciones/EspectroAM_IN.csv", 0, 10e3)
-# spectra_plot("ExpressInput/Mediciones/EspectroAM_ConRF.csv", 0, 10e3)
-# spectra_plot("ExpressInput/Mediciones/EspectroAM_SinFR.csv", 0, 10e3)
+spectra_plot("ExpressInput/Mediciones/EspectroAM_IN.csv", "ExpressInput/espectro_in.xml", "7_in", 2e3)
+spectra_plot("ExpressInput/Mediciones/EspectroAM_SinFR.csv", "ExpressInput/espectro_llave.xml", "7_llave", 10e3)
+spectra_plot("ExpressInput/Mediciones/EspectroAM_ConRF.csv", "ExpressInput/espectro_fr.xml", "7_out", 2e3)
