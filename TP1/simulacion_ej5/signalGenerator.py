77f6e81e5c11f82504e2e837a6ac10f4222fd035
rocioparra <rparra@itba.edu.ar>
2019-04-03 11:45:59 -0300

    empece a redactar el 7

--- TP1/simulacion_ej5/signalGenerator.py
+++ TP1/simulacion_ej5/signalGenerator.py
@@ -15,21 +15,21 @@ f0 = 500
 fexp = 10
 fp = 1.5e3
 
+SignalGenerator.GenerateCos(f0, 0, 2/f0, 2)
 
-
-SignalGenerator.GenerateCos(f0, 0, 10/f0, 3)
-
-SignalGenerator.GenerateCos(f0, 0, 10/f0, 1)
-SignalGenerator.GenerateCos(1e3, 0, 30*10/1e3, 1)
-SignalGenerator.GenerateCos(100, 0, 10/100, 1)
-
-SignalGenerator.GenerateExp(fexp, 0, 10/fexp, 2)
-
-SignalGenerator.Generate32Sine(500, 0, 10/500, 1)
-SignalGenerator.Generate32Sine(100, 0, 10/100, 1)
-
-SignalGenerator.GenerateAM(2*f0, 0.2*f0, 0, 10/(0.2*f0), 1/2)
-SignalGenerator.GenerateAM(2*0.8*fp, 2*0.08*f0, 0, 10/(2*0.08*fp), 1/2)
+# SignalGenerator.GenerateCos(f0, 0, 10/f0, 3)
+#
+# SignalGenerator.GenerateCos(f0, 0, 10/f0, 1)
+# SignalGenerator.GenerateCos(1e3, 0, 30*10/1e3, 1)
+# SignalGenerator.GenerateCos(100, 0, 10/100, 1)
+#
+# SignalGenerator.GenerateExp(fexp, 0, 10/fexp, 2)
+#
+# SignalGenerator.Generate32Sine(500, 0, 10/500, 1)
+# SignalGenerator.Generate32Sine(100, 0, 10/100, 1)
+#
+# SignalGenerator.GenerateAM(2*f0, 0.2*f0, 0, 10/(0.2*f0), 1/2)
+# SignalGenerator.GenerateAM(2*0.8*fp, 2*0.08*f0, 0, 10/(2*0.08*fp), 1/2)
 
 #SignalGenerator.GenerateSquare(0.001, 0.002)
 
