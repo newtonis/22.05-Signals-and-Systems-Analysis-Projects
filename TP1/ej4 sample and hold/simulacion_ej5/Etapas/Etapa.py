c63629cfcf84e43686eaf1da54bf5261f96cfb98
rocioparra <rparra@itba.edu.ar>
2019-04-03 13:36:59 -0300

    ahre

--- TP1/ej4 sample and hold/simulacion_ej5/Etapas/Etapa.py
+++
@@ -1,9 +0,0 @@
-from Etapas.Senial import Senial
-
-
-class Etapa:
-    def __init__(self, signalIn, signalOut):
-        pass
-
-    def processInput(self, signalIn):
-        return Senial([], [])
