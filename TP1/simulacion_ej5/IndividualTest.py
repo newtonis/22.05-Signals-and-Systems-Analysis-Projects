8a55e23ebc1cd6b5ecb5bc60785425f31b92bca4
jmestanza <jmestanza@itba.edu.ar>
2019-04-03 21:32:48 -0300

    added

--- TP1/simulacion_ej5/IndividualTest.py
+++ TP1/simulacion_ej5/IndividualTest.py
@@ -2,20 +2,22 @@ from IndividualTests.test_subplot_rochi import testsubplot as start
 from IndividualTests.subniquist2 import subniquistMediciones
 from IndividualTests.joaco_spectrum import joaco_spectrum
 from IndividualTests.sub_niquist1 import subniquistTest
-
+from IndividualTests.joaco_spectrum_2 import joaco_spectrum2
 from IndividualTests.bodes import makeBodes
 from IndividualTests.espectros import spectra_plot
 
 #joaco_spectrum()
 #makeBodes()
 #start()
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #subniquistTest()
-=======
+#=======
 
 
 # joaco_spectrum()
-spectra_plot("ExpressInput/Mediciones/EspectroAM_IN.csv", 0, 10e3)
+#spectra_plot("ExpressInput/Mediciones/EspectroAM_IN.csv", 0, 10e3)
 # spectra_plot("ExpressInput/Mediciones/EspectroAM_ConRF.csv", 0, 10e3)
 # spectra_plot("ExpressInput/Mediciones/EspectroAM_SinFR.csv", 0, 10e3)
->>>>>>> 9b51456fd0733f943ba9c7b64e1d82e319348978
+#>>>>>>> 9b51456fd0733f943ba9c7b64e1d82e319348978
+
+joaco_spectrum2()
