cd07c3d05304bc54fb97d2081000ca869645faa0
jmestanza <jmestanza@itba.edu.ar>
2019-04-20 19:05:55 -0300

    added read and write audio files

--- TP2/EJ2/main.py
+++ TP2/EJ2/main.py
@@ -1,11 +1,14 @@
 from synthesize_midi import *
 from utils import *
+import matplotlib.pyplot as plt
 
 fs = 44100
 track_synthesis = {"channel1":getBell}
-t,ytot = synthesize_midi("2hits16.mid",track_synthesis,fs)
+t,ytot = synthesize_midi("midi-samples/2hits16.mid",track_synthesis,fs)
+plt.plot(t,ytot)
+plt.show()
+
 sr,x = read('megalovania.mp3',normalized = False)
-
 print(x)
-
 write('output.mp3',sr,x,normalized = False)
+
