d4846ec64ac930bdcf9acd8b18044469d57f80e5
Ariel Nowik <newtonis.penguin@gmail.com>
2019-04-23 10:44:05 -0300

    adD

--- TP2/EJ1/test01/main.cpp
+++ TP2/EJ1/test01/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 #define pi 3.141592653589793238462643383279
 
+//float dpReal[2][4096], dpIm[2][4096], wReal[4096], wIm[4096];
+
+void show(vector <complex<float>> arr){
+    for (int i = 0;i < 6;i++){
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+}
 
 void getPow2(int pow2[],int log2n){
     pow2[0] = 1;
@@ -18,9 +26,98 @@ void generateWn(float wReal[], float wIm[],int n){
         wIm[i+n/2] = -wIm[i];
     }
 }
+unsigned int rev(unsigned int n,int maxn){
+    unsigned int rev = 0;
+    maxn /= 2;
+    while (n > 0){
+        if (n&1) {
+            rev |= maxn;
+        }
+        maxn /= 2;
+        n /= 2;
+    }
+    return rev;
+}
 
+void fftCormen(vector<complex<float>>& in, vector<complex<float>>& out){
+    int n = in.size();
+    int log2n = 0, v = 1;
+    while (v < n){
+        v *= 2;
+        log2n ++;
+    }
 
-void fftLenta(vector<complex<float>>& in, vector<complex<float>>& out){
+    static float arrReal[4096], arrComplex[4096];
+    //static *wReal = new float[n];
+    //static *wIm = new float[n];
+    static float wReal[4096], wIm[4096];
+    static bool generated = false;
+    //static bool generated[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
+
+    if (not generated) {
+        //cout << "generating ..." << '\n';
+        generateWn(wReal, wIm, 4096);
+        generated = true;
+    }
+
+    static int reverse[13][4096];
+    static int maxCalc = 1;
+    static int log2Calc = 0;
+    reverse[0][0] = 0;
+
+    while (maxCalc < n){
+        maxCalc *= 2;
+        log2Calc ++;
+
+        for (int j = 0;j < maxCalc;j++){
+            if (j < maxCalc / 2){
+                reverse[log2Calc][j] = reverse[log2Calc-1][j]*2;
+            }else{
+                reverse[log2Calc][j] = reverse[log2Calc][j-maxCalc/2] + 1;
+            }
+        }
+
+    }
+
+    for (int j = 0;j < n;j++){
+        int r = reverse[log2n][j];
+        arrReal[r] = in[j].real();
+        arrComplex[r] = in[j].imag();
+    }
+
+    int pow2 = 2;
+    int incFac = 4096 / n;
+
+    for (int i = 1;pow2 <= n;i++){
+        int m = pow2;
+        int u = n / pow2;
+        for (int k = 0;k < n;k += m){
+
+            for (int j = 0;j < m/2;j++){
+                float Wr = wReal[incFac*j*u%4096];
+                float Wi = wIm[incFac*j*u%4096];
+
+                float tReal = Wr * arrReal[k + j + m/2] - Wi * arrComplex[k + j + m/2];
+                float tComplex = Wi * arrReal[k + j + m/2] + Wr * arrComplex[k + j + m/2];
+
+                float uReal = arrReal[k + j];
+                float uComplex = arrComplex[k + j];
+
+                arrReal[k + j] = uReal + tReal;
+                arrComplex[k + j] = uComplex + tComplex;
+                arrReal[k + j + m/2] = uReal - tReal;
+                arrComplex[k + j + m/2] = uComplex - tComplex;
+            }
+        }
+        pow2 *= 2;
+    }
+    for (int i = 0;i < n;i++){
+        out[i].real(arrReal[i]);
+        out[i].imag(arrComplex[i]);
+    }
+}
+
+void fft(vector<complex<float>>& in, vector<complex<float>>& out){
     int n = in.size();
     int log2n = 0, v = 1;
     while (v < n){
@@ -70,11 +167,10 @@ void fftLenta(vector<complex<float>>& in, vector<complex<float>>& out){
 }
 
 
-float dpReal[2][4096], dpIm[2][4096], wReal[4096], wIm[4096];
 
 
 // fft vieja
-void fft(vector<complex<float>>& in, vector<complex<float>>& out){
+void fftGlobal(vector<complex<float>>& in, vector<complex<float>>& out){
     // 2^log2n = n sino no funciona
     // O(n) memoria
     // n/2*logn multiplicaciones
@@ -99,9 +195,12 @@ void fft(vector<complex<float>>& in, vector<complex<float>>& out){
     auto *wReal = new float[n];
     auto *wIm = new float[n];*/
 
+    float wReal[4096], wIm[4096];
+    float dpReal[2][4096], dpIm[2][4096];
+    int incFac = 4096 / n;
 
     getPow2(pow2, log2n);
-    generateWn(wReal, wIm, n);
+    generateWn(wReal, wIm, 4096);
 
     for (int j = 0;j < n;j++){
         dpReal[0][j] = in[j].real();
@@ -114,13 +213,13 @@ void fft(vector<complex<float>>& in, vector<complex<float>>& out){
 
         for (int j = 0;j < maxj;j++){
             for (int k = 0;k < maxk/2;k++){
-                float p1 = wReal[k*maxj%n]*dpReal[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
-                float p2 = wIm[k*maxj%n]*dpIm[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
+                float p1 = wReal[incFac*k*maxj%4096]*dpReal[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
+                float p2 = wIm[incFac*k*maxj%4096]*dpIm[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
 
                 dpReal[i%2][j + k*maxj] = dpReal[(i-1)%2][j+k%maxkLast*maxjLast] + p1 - p2;
 
-                float p3 = wReal[k*maxj%n]*dpIm[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
-                float p4 = wIm[k*maxj%n]*dpReal[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
+                float p3 = wReal[incFac*k*maxj%4096]*dpIm[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
+                float p4 = wIm[incFac*k*maxj%4096]*dpReal[(i-1)%2][j+maxj+k%maxkLast*maxjLast];
 
                 dpIm[i%2][j + k*maxj] = dpIm[(i-1)%2][j+k%maxkLast*maxjLast] + p3 + p4;
 
@@ -148,8 +247,17 @@ void fft(vector<complex<float>>& in, vector<complex<float>>& out){
 (-4-4j)
 (-4-9.65685424949238j)
  */
+
+
 int main() {
     vector <complex<float>> arr;
+    vector <complex<float>> arr2;
+    vector <complex<float>> out1(256);
+    vector <complex<float>> out2(4096);
+    vector <complex<float>> out3(256);
+    vector <complex<float>> out4(4096);
+    vector <complex<float>> out5(2048);
+
 //    arr.emplace_back(1);
 //    arr.emplace_back(2);
 //    arr.emplace_back(3);
@@ -160,37 +268,63 @@ int main() {
 //    arr.emplace_back(8);
 
     // medir tiempo
+//    generateWn(wReal, wIm, 4096);
+//
     for (int i = 0;i < 4096;i++){
-        arr.emplace_back(i);
+        arr.emplace_back(i);//i*454%(i+1)+1j);
     }
+    for (int i = 0;i < 256;i++){
+        arr2.emplace_back(i*454%(i+1)+1j);
+    }
+//
+//    clock_t begin = clock();
+//
+//    for (int i = 0;i < 1000;i++) {
+//        fft(arr, out1);
+//    }
+//
+//    clock_t end = clock();
+//    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+//
+//
+//    cout << "time fft 1 (stack) = " << elapsed_secs << '\n';
+//
+//    begin = clock();
+//
+    for (int i = 0;i < 1000;i++) {
+        fftGlobal(arr, out2);
+        fftGlobal(arr2, out1);
+    }
+    /*for (int i = 0;i < 1000;i++){
+        fftGlobal(arr2, out3);
+    }*/
+//
+//    end = clock();
+//    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+//
+//    cout << "time fft 2 (precálculos) = " << elapsed_secs << '\n';
+//
 
     clock_t begin = clock();
 
-    for (int i = 0;i < 100;i++) {
-        fft(arr, arr);
+    for (int i = 0;i < 1000;i++){
+        fftCormen(arr, out4);
+        fftCormen(arr2, out3);
     }
+    /*for (int i = 0;i < 1000;i++){
+        fftCormen(arr2, out5);
+    }*/
 
     clock_t end = clock();
+
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-
-    cout << "time global = " << elapsed_secs << '\n';
-
-    begin = clock();
-
-    for (int i = 0;i < 100;i++) {
-        fftLenta(arr, arr);
-    }
-
-    end = clock();
-    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-
-    cout << "time stack = " << elapsed_secs << '\n';
-
+    cout << "time fft 3 (cormen)= " << elapsed_secs << '\n';
     //fftOld(arrReal, arrIm, n, log2n, ansReal, ansIm);
 
-    //for (complex<float> item : arr){
-    //    cout<<item.real()<<" + "<<item.imag()<<"j"<<'\n';
-    //}
+    //show(out2);
+    //show(out4);
+    show(out3);
+    show(out1);
 
 }
