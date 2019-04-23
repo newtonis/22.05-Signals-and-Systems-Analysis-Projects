eb7c270042c5f3c030b0e3eb6eaa313f4ed85026
jmestanza <jmestanza@itba.edu.ar>
2019-04-23 12:48:43 -0300

    funciona agregar instrumentos

--- TP2/EJ2/synth_utils.py
+++ TP2/EJ2/synth_utils.py
@@ -1,5 +1,6 @@
 import mido
 from numpy import *
+
 def noteToFreq(note):
     a = 440 #frequency of A (coomon value is 440Hz)
     return (a / 32) * (2 ** ((note - 9) / 12))
@@ -28,7 +29,7 @@ class individual_track:
     time_arr = None
     total_time = None
     amp_arr = None
-    def __init__(self,ticks_per_beat_,function_,fs):
+    def __init__(self,ticks_per_beat_, fs):
         self.tempo = None
         self.ticks_per_beat = ticks_per_beat_
         self.bpm = None
@@ -37,19 +38,12 @@ class individual_track:
         self.t_off = {}
         self.noteParams = None
         self.fs = fs
-        self.function = function_
+        self.function = None
         self.total_time = None
         self.amp_arr = None
         self.time_arr=None
 
-    def setFirstTrack(self,track):
-        for MetaMessage in track:
-            if(MetaMessage.type == "set_tempo"):
-                self.bpm = mido.tempo2bpm(MetaMessage.tempo)
-                self.tempo = MetaMessage.tempo
-    def setTrack(self,track,bpm,tempo):
-        self.bpm = bpm
-        self.tempo = tempo
+    def getNotes(self,track):
         for message in track:
             self.time_counter+=message.time
             if message.type == "note_on" and message.velocity != 0:
@@ -61,18 +55,18 @@ class individual_track:
                     self.t_off[message.note] = []
                 self.t_off[message.note].append(noteParams(message.velocity,message.note,self.time_counter))
 
+    def getAmpArr(self,bpm,tempo):
+        self.bpm = bpm
+        self.tempo = tempo
         # ahora quiero obtener para cada nota un delta t dado por t_on y t_off
-
         for note, note_param_arr in self.t_on.items():  # note es la key del dict
             for i in range(len(note_param_arr)):
-                note_param_arr[i].delta_t = mido.tick2second(abs(self.t_on[note][i].time - self.t_off[note][i].time), self.ticks_per_beat, self.tempo)
-
+                note_param_arr[i].delta_t = mido.tick2second(abs(self.t_on[note][i].time - self.t_off[note][i].time),
+                                                             self.ticks_per_beat, self.tempo)
 
         self.total_time = mido.tick2second(self.time_counter,self.ticks_per_beat,self.tempo)
-
         self.time_arr = arange(0, self.total_time, 1 / self.fs)
         self.amp_arr = zeros(len(self.time_arr))
-        segs_per_tick = mido.tick2second(1,self.ticks_per_beat, self.tempo)
 
         for notes, note_param_arr in self.t_on.items():
             for nparam in note_param_arr:
--- TP2/EJ2/synthesize_midi.py
+++ TP2/EJ2/synthesize_midi.py
@@ -14,17 +14,41 @@ def synthesize_midi( midiFilename ,tracks_synthesis ,fs):
     bpm = 0
     tempo = 0
     track_list=[]
-    for channel,function in tracks_synthesis.items():
-        for index,track in enumerate(midi_file.tracks):
-            if(index == 0):
-                track_list.append(individual_track(ticks_per_beat,function,fs)) #aca function no pincha ni corta
-                track_list[0].setFirstTrack(track)
-                bpm = track_list[0].bpm
-                tempo = track_list[0].tempo
-            if(index>0):
-                track_list.append(individual_track(ticks_per_beat,function,fs))
-                track_list[index].setTrack(track,bpm,tempo)
+    for index,track in enumerate(midi_file.tracks):
+        for MetaMessage in track:
+            if (MetaMessage.type == "set_tempo"):
+                bpm = mido.tempo2bpm(MetaMessage.tempo)
+                tempo = MetaMessage.tempo
+            else:
+                bpm = 130.1
+                tempo = mido.bpm2tempo(bpm)
 
+        track_list.append(individual_track(ticks_per_beat,fs))
+        track_list[index].getNotes(track) # no hace nada si no hay note ons y off
+        if(len(track_list[index].t_on)>0):
+            channel = "channel"+str(index)
+            track_list[index].function=tracks_synthesis[channel]
+
+
+    for track_el in track_list:
+        if(len(track_el.t_on)>0): #esto significa que hay al menos una nota
+            track_el.getAmpArr(bpm,tempo)
+
+    total_t_arr = 0
+    total_amp_arr = 0
+    j = 0
+    for index,track_el in enumerate(track_list):
+        if (len(track_el.t_on) > 0):  # esto significa que hay al menos una nota
+            if(j==0):
+                total_amp_arr = track_el.amp_arr.copy()
+                total_t_arr = track_el.time_arr
+                j+=1
+            else:
+                total_amp_arr+=track_el.amp_arr
+                j+=1
+
+    max = abs(amax(total_amp_arr))
+    total_amp_arr = divide(total_amp_arr, max)
     # ACA FALTARIA SUMAR LOS TRACKLIST [i]
-    return track_list[1].time_arr,track_list[1].amp_arr
+    return total_t_arr,total_amp_arr
 
