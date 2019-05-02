bb4900b77863c1d3a5724bbb606270673e7bb32a
jmestanza <jmestanza@itba.edu.ar>
2019-05-02 14:25:11 -0300

    ya no tira canales que no tienen nota

--- TP2/Software/ProcessMidi/getMidiMetadata.py
+++ TP2/Software/ProcessMidi/getMidiMetadata.py
@@ -6,12 +6,18 @@ def getMidiMetadata(filename):
 
     midi_file = MidiFile(filename)
     for j, track in enumerate(midi_file.tracks):
-        data.append(
-            {
-                "name": "Canal " + str(j),
-                "id": j
-            }
-        )
+        t_on = []
+        time_counter = 0
+        for index, message in enumerate(track):
+            if message.type == "note_on" and message.velocity != 0:
+                t_on.append([time_counter, message.velocity, message.note])
+        if len(t_on) > 0:
+            data.append(
+                {
+                    "name": "Canal " + str(j),
+                    "id": j
+                }
+            )
 
     return data
 
